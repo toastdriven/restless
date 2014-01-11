4be1df9e0bc25ee1d9cdbc66bc0645c9711915e6
Daniel Lindsley <daniel@toastdriven.com>
2014-01-11 10:38:38 -0800

    Better messages on exceptions.

--- restless/exceptions.py
+++ restless/exceptions.py
@@ -2,13 +2,27 @@ class RestlessError(Exception):
     pass
 
 
-class NotFound(RestlessError):
+class HttpError(RestlessError):
+    status = 500
+    msg = "Application Error"
+
+    def __init__(self, msg=None):
+        if not msg:
+            msg = self.__class__.msg
+
+        super(HttpError, self).__init__(msg)
+
+
+class NotFound(HttpError):
     status = 404
+    msg = "Resource not found."
 
 
-class MethodNotAllowed(RestlessError):
+class MethodNotAllowed(HttpError):
     status = 405
+    msg = "The specified HTTP method is not allowed."
 
 
-class MethodNotImplemented(RestlessError):
+class MethodNotImplemented(HttpError):
     status = 501
+    msg = "The specified HTTP method is not implemented."
