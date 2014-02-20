ae5a9cb66b382cc166a73f6261432c228031d120
Daniel Lindsley <daniel@toastdriven.com>
2014-02-20 07:26:02 -0800

    Changed how exceptions are handled & added serialized tracebacks.
    This makes working in development much more consistent with production.
    You'll get the same responses (with traceback) in dev as what would be
    seen in production. See the tutorial for specifics.
    
    Thanks to @jphalip for an early version of this & the traceback code.

--- restless/utils.py
+++ restless/utils.py
@@ -1,5 +1,6 @@
 import datetime
 import decimal
+import traceback
 
 try:
     import json
@@ -82,3 +83,15 @@ class MoreTypesJSONEncoder(json.JSONEncoder):
             return str(data)
         else:
             return super(MoreTypesJSONEncoder, self).default(data)
+
+
+def format_traceback(exc_info):
+    stack = traceback.format_stack()
+    stack = stack[:-2]
+    stack.extend(traceback.format_tb(exc_info[2]))
+    stack.extend(traceback.format_exception_only(exc_info[0], exc_info[1]))
+    stack_str = "Traceback (most recent call last):\n"
+    stack_str += "".join(stack)
+    # Remove the last \n
+    stack_str = stack_str[:-1]
+    return stack_str
