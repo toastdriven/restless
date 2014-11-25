fc91e8e8a09e596391a6fed8d04a3a48fab37efe
Daniel Lindsley <daniel@toastdriven.com>
2014-11-24 20:05:42 -0600

    Fixed a documentation typo in the Pyramid code.
    Fixes #43. Thanks for the report, harnash!

--- restless/pyr.py
+++ restless/pyr.py
@@ -42,7 +42,7 @@ class PyramidResource(Resource):
         :param routename_prefix: (Optional) A prefix for the URL's name (for
             resolving). The default is ``None``, which will autocreate a prefix
             based on the class name. Ex: ``BlogPostResource`` ->
-            ``api_blog_post_list``
+            ``api_blogpost_list``
         :type routename_prefix: string
 
         :returns: The final name
